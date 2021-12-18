e444e229c68c8f86fa5e0f146517841c2a47a8b7
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2021-12-18 23:46:55 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int()
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = 16349774
+    API_HASH = "fa09cf6cdfdc16a315cafca62909857e"
+    BOT_TOKEN = "5099273659:AAGPc7_t0yd5tzFdcoHUeHprTG3UjJgTULo"
