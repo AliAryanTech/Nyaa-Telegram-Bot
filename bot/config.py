d1614ed218fd9f413d046eec61978df269b325b6
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2022-01-07 01:20:05 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    API_ID = int id
-    API_HASH = ""
-    BOT_TOKEN = ""
+    API_ID = 16349774
+    API_HASH = "fa09cf6cdfdc16a315cafca62909857e"
+    BOT_TOKEN = "5066813905:AAFrzFykRrVB5DP5akRi5bI0SDKEitJMiPI"
